7333675564e06f9b4daa55c3f2f0c99c1090c220
Iuri Mendes <fcoiuri@gmail.com>
2022-04-19 00:46:05 -0300

    feature: add cep promise and state.

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState, ChangeEvent  } from "react";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import cep from "cep-promise";
 
 const primary = "#FFF";
 const useStyles = makeStyles((theme: Theme) =>
-createStyles({
+  createStyles({
     title: {
-        fontSize: 32,
-        textAlign: 'center',
-        marginTop: "2%",
-        fontFamily: "Akshar, sans-serif",
+      fontSize: 32,
+      textAlign: "center",
+      marginTop: "2%",
+      fontFamily: "Akshar, sans-serif",
     },
     form: {
-        "& > *": {
+      "& > *": {
         margin: theme.spacing(1),
         width: "35ch",
       },
@@ -34,21 +35,28 @@ createStyles({
       color: `${primary} !important`,
       "&::-webkit-outer-spin-button, &::-webkit-inner-spin-button": {
         "-webkit-appearance": "none",
-        margin: 0
-      }
+        margin: 0,
+      },
     },
   })
 );
 
 export const Home: React.FC = function () {
+  const [CEP, setCEP] = useState("");
   const classes = useStyles();
+  console.log(CEP)
+  cep(CEP).then(console.log)
+
+  const handleChangeCep = (e: ChangeEvent<HTMLInputElement>) => {
+    setCEP(e.target.value);
+  }
   return (
     <React.Fragment>
-        <div className={classes.title}>
-        Achar endereço pelo CEP
-        </div>
+      <div className={classes.title}>Achar endereço pelo CEP</div>
       <form className={classes.form} noValidate autoComplete="off">
         <TextField
+          value={CEP}
+          onChange={handleChangeCep}
           inputProps={{ className: classes.textField }}
           InputLabelProps={{ className: classes.textField }}
           id="cep"
