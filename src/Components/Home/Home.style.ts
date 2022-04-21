f61fbfa2e866c431f66c24471180d766d0ddf47c
Iuri Mendes <fcoiuri@gmail.com>
2022-04-21 20:47:37 -0300

    feature: finishing find

---
+++ src/Components/Home/Home.style.ts
@@ -0,0 +1,91 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+const primary = '#FFF';
+const secondary = '#202020';
+const marginTitle = '2%'
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    title: {
+      fontSize: 32,
+      textAlign: 'center',
+      marginTop: marginTitle,
+      fontFamily: 'Akshar, sans-serif',
+    },
+    find: {
+      '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+          borderColor: primary,
+        },
+        '&:hover fieldset': {
+          borderColor: primary,
+        },
+        '&.Mui-focused fieldset': {
+          borderColor: primary,
+        },
+      },
+      textAlign: 'center',
+      marginTop: marginTitle,
+      // margin: '1%'
+    },
+    form: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '35ch',
+      },
+      '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+          borderColor: primary,
+        },
+        '&:hover fieldset': {
+          borderColor: primary,
+        },
+        '&.Mui-focused fieldset': {
+          borderColor: primary,
+        },
+      },
+      textAlign: 'center',
+      marginTop: '1%',
+    },
+    textField: {
+      color: `${primary} !important`,
+      '&::-webkit-outer-spin-button, &::-webkit-inner-spin-button': {
+        '-webkit-appearance': 'none',
+      },
+      display: 'inline-block',
+    },
+    InputMask: {
+      underline: {
+        '&&&:before': {
+          borderBottom: 'none'
+        },
+        '&&:after': {
+          borderBottom: 'none'
+        }
+      }
+
+    },
+    wrapper: {
+      textAlign: 'center',
+    },
+    info: {
+      padding: '2%',
+      borderRadius: 25,
+      margin: '2%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      textAlign: 'center',
+      width: '25%',
+      backgroundColor: primary,
+      color: secondary,
+      display: 'inline-block',
+    },
+    botaoGrid:{
+      marginTop: marginTitle,
+    },
+    botao: {
+      backgroundColor: primary,
+      color: secondary,
+      fontWeight: 'bolder',
+      marginLeft: '5%'
+    },
+  })
+);
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,89 +1,124 @@
 import React, { useState, ChangeEvent } from "react";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
+import { Grid, TextField, Button } from "@material-ui/core";
 import cep from "cep-promise";
 // @ts-ignore
 import brazilianStates from "brazilian-states";
-
-const primary = "#FFF";
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    title: {
-      fontSize: 32,
-      textAlign: "center",
-      marginTop: "2%",
-      fontFamily: "Akshar, sans-serif",
-    },
-    form: {
-      "& > *": {
-        margin: theme.spacing(1),
-        width: "35ch",
-      },
-      "& .MuiOutlinedInput-root": {
-        "& fieldset": {
-          borderColor: primary,
-        },
-        "&:hover fieldset": {
-          borderColor: primary,
-        },
-        "&.Mui-focused fieldset": {
-          borderColor: primary,
-        },
-      },
-      textAlign: "center",
-      marginTop: "1%",
-    },
-    textField: {
-      color: `${primary} !important`,
-      "&::-webkit-outer-spin-button, &::-webkit-inner-spin-button": {
-        "-webkit-appearance": "none",
-        margin: 0,
-      },
-    },
-  })
-);
+import { useStyles } from "./Home.style";
+// @ts-ignore
+import InputMask from "react-input-mask";
 
 export const Home: React.FC = function () {
+  const [info, setInfo] = useState(false);
+  const [notFound, setNotFound] = useState(false);
   const [CEP, setCEP] = useState("");
   const [city, setCity] = useState("");
   const [neighborhood, setNeighborhood] = useState("");
   const [state, setState] = useState("");
   const [street, setStreet] = useState("");
   const classes = useStyles();
-  //   console.log(CEP)
 
   const handleChangeCEP = (e: ChangeEvent<HTMLInputElement>) => {
     setCEP(e.target.value);
-    cep(e.target.value).then((value: any) => {
-      setCity(value.city);
-      setNeighborhood(value.neighborhood);
-      setState(brazilianStates.get(value.state).name);
-      setStreet(value.street);
-      console.log(value);
-      console.log(`Cidade: ${city}`);
-    });
-};
-return (
+    // setCEP(
+    //   (e.target.value = Math.max(0, parseInt(e.target.value))
+    //     .toString()
+    //     .slice(0, 9))
+    // );
+  };
+  const showNotFound = () => {
+    setNotFound(true);
+    setInfo(false);
+  };
+  const showCEP = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    cep(CEP)
+      .then((value: any) => {
+        // setCEP(CEP.substring(0, 5) + "-" + CEP.substring(5));
+        setCity(value.city);
+        setNeighborhood(value.neighborhood);
+        setState(brazilianStates.get(value.state).name);
+        setStreet(value.street);
+        setInfo(true);
+        setNotFound(false);
+      })
+      .catch(showNotFound());
+  };
+  return (
     <React.Fragment>
       <div className={classes.title}>Achar endereço pelo CEP</div>
-      <form className={classes.form} noValidate autoComplete="off">
-        <TextField
-          value={CEP}
-          onChange={handleChangeCEP}
-          inputProps={{ className: classes.textField }}
-          InputLabelProps={{ className: classes.textField }}
-          id="cep"
-          type="number"
-          label="Digite o CEP"
-          variant="outlined"
-        />
-      </form>
-      <div>
-        <div>Cidade: {city}</div>
-        <div>Estado: {state}</div>
-        <div>Bairro: {neighborhood}</div>
-        <div>Rua: {street}</div>
-      </div>
+      <Grid container justifyContent="center">
+        <Grid item className={classes.find}>
+          <TextField
+            value={CEP}
+            onChange={handleChangeCEP}
+            inputProps={{ className: classes.textField }}
+            InputLabelProps={{ className: classes.textField }}
+            id="cep"
+            label="Digite o CEP"
+            variant="outlined"
+          />
+          {/* <InputMask mask="99999-999" value={CEP} onChange={handleChangeCEP}>
+            {
+                () => (
+                    <TextField
+                        label="Phone"
+                        variant="outlined"
+                        type="text"
+                        fullWidth
+                        required
+                    />
+                )
+            }
+        </InputMask> */}
+          {/* <InputMask
+            mask="99999-999"
+            value={CEP}
+            id="cep"
+            onChange={handleChangeCEP}
+            InputProps={{ disableUnderline: true }}
+          >
+            {() => (
+              <TextField
+                label="Digite o CEP"
+                id="cep"
+                variant="outlined"
+                inputProps={{ className: classes.textField }}
+                InputLabelProps={{ className: classes.textField }}
+                margin="normal"
+              />
+            )}
+          </InputMask> */}
+        </Grid>
+        <Grid
+          item
+          // alignItems="stretch"
+          style={{ display: "flex" }}
+          className={classes.botaoGrid}
+        >
+          <Button
+            variant="contained"
+            className={classes.botao}
+            onClick={showCEP}
+          >
+            Buscar
+          </Button>
+        </Grid>
+      </Grid>
+      {info && (
+        <div className={classes.wrapper}>
+          <div className={classes.info}>
+            <div>Cidade: {city}</div>
+            <div>Estado: {state}</div>
+            <div>Bairro: {neighborhood}</div>
+            <div>Rua: {street}</div>
+          </div>
+        </div>
+      )}
+      {notFound && (
+        <div className={classes.wrapper}>
+          <div className={classes.info}>CEP não encontrado</div>
+        </div>
+      )}
     </React.Fragment>
   );
 };
